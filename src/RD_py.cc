f824ee6d124ef5ecb0c1bef2f88fc03356b75482
Zhou Huang <787074306@qq.com>
2023-08-06 20:34:17 +0800

    init bug fix

--- src/RD_py.cc
+++ src/RD_py.cc
@@ -1,6 +1,8 @@
 #include "XTPReader.hh"
 
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/complex.h>
 
 namespace py = pybind11;
 
@@ -9,7 +11,7 @@ PYBIND11_MODULE(XTPReader, m) {
     m.doc() = "pybind11 module for xtp data reading";
 
     py::class_< XTPReader >(m, "XTPReader")
-        .def(py::init<const char*, int, char, int, bool>())
+        .def(py::init<const char*, int, int, int, bool>())
         .def("read_csv", &XTPReader::read_csv)
         .def("get_stocks_code_list", &XTPReader::get_stocks_code_list)
         .def("get_stock_data", &XTPReader::get_stock_data)
