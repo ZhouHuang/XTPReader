50969e8dd9a86c8b22cb789e6e00bc683c4f53b3
Zhou Huang <787074306@qq.com>
2023-08-07 19:04:38 +0800

    add py demo

--- test/simulate.py
+++ test/simulate.py
@@ -1,10 +1,31 @@
 import pandas as pd 
 import numpy as np 
 
-from XTPReader_c import XTPReader
+from XTPReader import XTPReader
 
 def main():
-    pass
+    path = "./test/demo_t.csv"
+    
+    code_col = 6
+    header=-1
+    skiprows=2
+    rd = XTPReader(path, code_col, header, skiprows)
+
+    rd.read_csv()
+    stk_list = rd.get_stocks_code_list()
+
+    for code in stk_list:
+        data = rd.get_stock_data(code)
+        for line in data:
+            for content in line:
+                print(content, end=" ")
+            print()
+
+
+    out_path = './t1'
+    code = '000629'
+    dt = '20230801'
+    rd.to_csv(out_path, code, dt)
 
 if __name__ == '__main__':
     main()
