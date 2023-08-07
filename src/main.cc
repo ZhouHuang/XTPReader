ea8a981a81b7a6a8770dad6b4181223abaa7001d
Zhou Huang <787074306@qq.com>
2023-08-07 22:18:54 +0800

    update

--- src/main.cc
+++ src/main.cc
@@ -4,25 +4,36 @@
 using std::cout;
 using std::endl;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     const string path = "./test/demo_t.csv";
-    
+
     int code_col = 6;
-    int header=-1;
-    int skiprows=2;
+    int header = -1;
+    int skiprows = 2;
     XTPReader rd(path.c_str(), code_col, header, skiprows);
 
-    rd.read_csv();
+    int ex = 2;
+    rd.read_csv(ex);
     auto stk_list = rd.get_stocks_code_list();
 
-    for(const auto& code : stk_list) {
+    for (const auto &code : stk_list) {
+        printf("code %s\n", code.c_str());
         auto data = rd.get_stock_data(code);
-        for(const auto& line: data) {
-            for(const auto& content : line) {
+        for (const auto &line : data) {
+            for (const auto &content : line) {
                 cout << content << " ";
             }
             cout << endl;
         }
+    }
+
+    const char* out_path = "./";
+    string dt = "20230803";
+
+    for (const auto &code : stk_list) {
+
+        printf("code %s\n", code.c_str());
+        rd.to_csv(out_path, code, dt);
     }
 
     return 0;
